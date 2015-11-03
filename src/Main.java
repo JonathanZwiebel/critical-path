5a1a9b3880899c82fa9be07e3d53135f559e2920
JonathanZwiebel <jonathanzwiebel@gmail.com>
2015-11-03 10:49:34 -0800

    Tested and verified that JSFML working correctly

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,42 @@
+import org.jsfml.graphics.Color;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event;
+
 /**
  * Created by Jonathan Zwiebel
  */
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello Main");
+        RenderWindow rw = new RenderWindow();
+        rw.create(new VideoMode(1000, 1000), "Render Window");
+        rw.setFramerateLimit(30);
+
+        Color color = Color.CYAN;
+
+        while(rw.isOpen()) {
+            rw.clear(color);
+
+            rw.display();
+
+            for(Event event : rw.pollEvents()) {
+                switch(event.type) {
+                    case CLOSED:
+                        rw.close();
+                        break;
+                    case MOUSE_BUTTON_PRESSED:
+                        color = Color.GREEN;
+                        break;
+                    case MOUSE_BUTTON_RELEASED:
+                        color = Color.CYAN;
+                        break;
+                    case MOUSE_LEFT:
+                        color = Color.CYAN;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
 }

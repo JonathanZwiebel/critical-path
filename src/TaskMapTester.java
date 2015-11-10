d1cdd7d1c11afd3dfed9a723d3794b62d577acfc
JonathanZwiebel <jonathanzwiebel@gmail.com>
2015-11-10 11:36:43 -0800

    Added the Spaghetti test

--- src/TaskMapTester.java
+++ src/TaskMapTester.java
@@ -22,4 +22,11 @@ public class TaskMapTester {
         project.put("D", 16, new String[]{"C"}, new String[]{"E"});
         project.put("E", 1.5f, new String[]{"B", "D"}, new String[]{});
     }
+
+    // http://www.lindsay-sherwin.co.uk/project_framework/images/cpa_spaghetti.jpg
+    public static void spaghettiTest(TaskMap project) {
+        project.put("Get Ingrediants", 5, new String[]{}, new String[]{"Cook Spaghetti", "Prepare Egg Sauce", "Cook Bacon"});
+
+
+    }
 }
